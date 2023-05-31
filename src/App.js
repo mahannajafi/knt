c7d6c98400451df4ee890e8d24057573c26d094d
Elham_Zahir <elham.zahirk@gmail.com>
2023-05-31 20:24:25 +0430

    choose address

--- src/App.js
+++ src/App.js
@@ -9,22 +9,13 @@ import MyOrders from "./Pages/MyOrders";
 import MyAddresses from "./Pages/MyAddresses";
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MiniNavbar from './components/MiniNavbar/MiniNavbar';
-import Footer from './components/Footer/Footer';
-// import SignUp from "./components/SignUp";
-// import SendCode from "./components/SendCode";
-// import ChoosePass from "./components/ChoosePass";
-// import Login from "./components/Login";
 import Login from "./Pages/Login";
-// import ForgetPassForm from "./components/ForgetPass";
 import ForgetPass from "./Pages/ForgetPass";
 import RecoveryPass from "./Pages/RecoveryPass";
 import Register from "./Pages/Register";
 import VerificationCode from "./Pages/VereficationCode";
 import SetPass from "./Pages/SetPass";
 import ProviderLogin from "./Pages/ProviderLogin";
-// import ResetPassCode from "./components/ResetPassCode";
-// import ResetPass from "./components/ResetPass";
 import DesignerDashboard from "./components/Dashboard/DesignerDashboard";
 import Learn from './components/Learn/Learn';
 import ChooseSingleProduct from './Pages/ChooseSingleProduct';
@@ -39,13 +30,8 @@ function App() {
           <Route path='' element={<Home />} />
           <Route path='*' element={<Home />} />
           <Route path='SingleProduct' element={<SingleProduct />} />
-
           <Route path='SingleRawProduct' element={<SingleRawProduct />} />
-
           <Route path='Design' element={<Design />} />
-          {/*<Route path="SignUp" element={<SignUp />}></Route>*/}
-          {/*<Route path="SendCode" element={<SendCode />}></Route>*/}
-          {/*<Route path="ChoosePass" element={<ChoosePass />}></Route>*/}
           <Route path="Login" element={<Login />}></Route>
           <Route path="ForgetPass" element={<ForgetPass />}></Route>
           <Route path="RecoveryPass" element={<RecoveryPass />}></Route>
--- src/components/ChooseAddress/SelectAddress.jsx
+++ src/components/ChooseAddress/SelectAddress.jsx
@@ -1,12 +1,57 @@
+import React , {useState} from "react";
 import "./SelectAddress.css"
+import Address from "../Addresses/Address";
 
 function SelectAddress() {
+    const [selectedAddress , setSelectedAddress] = useState(1)
+    const [selectedDay , setSelectedDay] = useState(1)
+    const [selectedTime , setSelectedTime] = useState(1)
+
     return (
         <div className="selectAddress">
             <div className="buyLevels">
                 <div className="buyLevel">سبد خرید</div>
                 <div className="buyLevel">انتخاب آدرس و زمان ارسال</div>
                 <div className="buyLevel">پرداخت</div>
+            </div>
+            <h3>انتخاب آدرس</h3>
+            <div className="addressSection"  onClick={()=>setSelectedAddress(1)}>
+                <div className="selectedAddress" style={{borderColor: (selectedAddress === 1) && "#234E70FF"}}></div>
+                <Address />
+            </div>
+            <div className="addressSection" onClick={()=>setSelectedAddress(2)}>
+                <div className="selectedAddress" style={{borderColor: (selectedAddress === 2) && "#234E70FF"}}></div>
+                <Address />
+            </div>
+            <div className="chooseDay">
+                <h3>انتخاب زمان ارسال</h3>
+                <div className='days'>
+                    <div className="day" style={{backgroundColor: (selectedDay === 1) && "#234E70FF",
+                    color: (selectedDay === 1) && "#FBF8BEFF"}} onClick={()=>setSelectedDay(1)}>
+                        {"دوشنبه 22"}</div>
+                    <div className="day" style={{backgroundColor: (selectedDay === 2) && "#234E70FF",
+                        color: (selectedDay === 2) && "#FBF8BEFF"}} onClick={()=>setSelectedDay(2)}>
+                        {"سه شنبه 23"}</div>
+                </div>
+                {selectedDay === 1 ?
+                    <>
+                        <div className="chooseTime"><div className="selectDay" style={{borderColor: (selectedTime === 1) && "#234E70FF"}}
+                                                         onClick={()=>setSelectedTime(1)}></div>{"ساعت 15 الی 18"}</div>
+                        <div className="borderForDays"></div>
+                        <div className="chooseTime"><div className="selectDay" style={{borderColor: (selectedTime === 2) && "#234E70FF"}}
+                                                         onClick={()=>setSelectedTime(2)}></div>{"ساعت 15 الی 18"}</div>
+                        <div className="borderForDays"></div>
+                        <div className="chooseTime"><div className="selectDay" style={{borderColor: (selectedTime === 3) && "#234E70FF"}}
+                                                         onClick={()=>setSelectedTime(3)}></div>{"ساعت 15 الی 18"}</div>
+                    </> :
+                    <>
+                        <div className="chooseTime"><div className="selectDay" style={{borderColor: (selectedTime === 1) && "#234E70FF"}}
+                                                         onClick={()=>setSelectedTime(1)}></div>{"ساعت 15 الی 18"}</div>
+                        <div className="borderForDays"></div>
+                        <div className="chooseTime"><div className="selectDay" style={{borderColor: (selectedTime === 2) && "#234E70FF"}}
+                                                         onClick={()=>setSelectedTime(2)}></div>{"ساعت 15 الی 18"}</div>
+                    </>}
+
             </div>
         </div>
     )
